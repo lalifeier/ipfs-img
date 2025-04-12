947802e946e2d0da6d80d70a352977d8650c84fb
lalifeier <lalifeier@gmail.com>
2025-04-12 13:59:55 +0000

    footer加上  -基于IPFS的文件存储服务

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-background border-t p-4">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} IPFS Uploader by lalifeier. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} IPFS Uploader by lalifeier. All rights reserved. -基于IPFS的文件存储服务</p>
       </div>
     </footer>
   );
