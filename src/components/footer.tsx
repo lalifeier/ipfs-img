19a881083b8ae48c902da5877b88fa43d33164ce
lalifeier <lalifeier@gmail.com>
2025-04-12 13:54:56 +0000

    header应该是显示  IPFS Uploader   footer增加作者 lalifeier

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,10 +5,11 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-background border-t p-4">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} FileDrop. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} IPFS Uploader by lalifeier. All rights reserved.</p>
       </div>
     </footer>
   );
 };
 
 export default Footer;
+
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,11 @@ const Header: React.FC = () => {
   return (
     <header className="bg-background border-b p-4">
       <div className="container mx-auto flex items-center justify-between">
-        <h1 className="text-xl font-semibold">FileDrop</h1>
+        <h1 className="text-xl font-semibold">IPFS Uploader</h1>
       </div>
     </header>
   );
 };
 
 export default Header;
+
